96c5e66345980df38ab9a7ef5999b8ce3b993c32
Toni Dezman <toni.dezman@gmail.com>
2019-10-13 14:51:20 +0200

    cleans up dummy expense text

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -36,124 +36,39 @@ RSpec.describe Expense, type: :model do
 
   def expenses(current_date: DateTime.now.beginning_of_month + 20.days)
     [
-      [
-        'Bank inc.',
-        '12.10.2019',
-        'VI-ONLINE CITYPARK, LJUBLJANA',
-        '-75,41 EUR'
-      ],
-      ['Bank inc.', '11.10.2019', 'VI-HOFER hvala, Ljubljana', '-13,04 EUR'],
-      ['Bank inc.', '09.10.2019', 'VI-INTERSPAR VIC, LJUBLJANA', '-22,77 EUR'],
-      ['Bank inc.', '05.10.2019', 'VI-HOFER hvala, Ljubljana', '-5,74 EUR'],
-      ['Bank inc.', '04.10.2019', 'VI-HOFER hvala, Ljubljana', '-50,98 EUR'],
-      [
-        'Bank inc.',
-        '03.10.2019',
-        'VI-PE MUELLER COPOVA, LJUBLJANA',
-        '-41,59 EUR'
-      ],
-      [
-        'Bank inc.',
-        '01.10.2019',
-        'VI-LECLERC RUDNIDIS, LJUBLJANA',
-        '-84,00 EUR'
-      ],
-      [
-        'Bank inc.',
-        '01.10.2019',
-        'VI-C & A Ljubljana, Ljubljana',
-        '-42,60 EUR'
-      ],
-      [
-        'Bank inc.',
-        '01.10.2019',
-        'VI-ONLINE CITYPARK, LJUBLJANA',
-        '-62,59 EUR'
-      ],
-      ['Bank inc.', '01.10.2019', 'Obresti pozitivnega stanja', '0,01 EUR'],
-      [
-        'Bank inc.',
-        '30.09.2019',
-        'Nad. za vodenje kart.rač. za AC Visa Inspire',
-        '-0,10 EUR'
-      ],
-      [
-        'Bank inc.',
-        '30.09.2019',
-        'Nadomestilo vodenja TRR e-račun',
-        '-1,70 EUR'
-      ],
-      [
-        'Bank inc.',
-        '30.09.2019',
-        'Nadomestilo za uporabo Banke IN',
-        '-1,07 EUR'
-      ],
-      ['VZAJEMNA D.V.Z.', '30.09.2019', 'VZAJEMNA - 480368890', '-34,60 EUR'],
-      [
-        'Bank inc.',
-        '30.09.2019',
-        'Plačilna transakcija s SEPA direktno obremenitvijo',
-        '-0,33 EUR'
-      ],
-      [
-        'Bank inc.',
-        '30.09.2019',
-        'Plačilna transakcija s SEPA direktno obremenitvijo',
-        '-0,33 EUR'
-      ],
-      ['VRTEC VISKI GAJ', '30.09.2019', 'OSKRBNINE', '-169,63 EUR'],
-      ['Bank inc.', '28.09.2019', 'VI-VERACE 2.0, LJUBLJANA', '-31,50 EUR'],
-      ['Bank inc.', '28.09.2019', 'VI-HOFER hvala, Ljubljana', '-4,52 EUR'],
-      [
-        'Bank inc.',
-        '25.09.2019',
-        'VI-ALPE PANON PE LJ COPOVA, LJUBLJANA',
-        '-11,40 EUR'
-      ],
-      ['Bank inc.', '25.09.2019', 'VI-SPAR COPOVA, LJUBLJANA', '-2,01 EUR'],
-      ['Bank inc.', '24.09.2019', 'VI-HOFER hvala, Ljubljana', '-20,92 EUR'],
-      [
-        'Bank inc.',
-        '24.09.2019',
-        'VI-ALPE PANON PE LJ COPOVA, LJUBLJANA',
-        '-1,70 EUR'
-      ],
-      ['MARJETA TURK', '24.09.2019', 'Drugo', '450,78 EUR'],
-      [
-        'Bank inc.',
-        '23.09.2019',
-        'VI-ALPE PANON PE LJ COPOVA, LJUBLJANA',
-        '-3,40 EUR'
-      ],
-      [
-        'Bank inc.',
-        '21.09.2019',
-        'VI-V. MESTO ATLANTIS REC., LJUBLJANA',
-        '-68,00 EUR'
-      ],
-      [
-        'Bank inc.',
-        '21.09.2019',
-        'VI-ALPE PANON PE LJ SMARTINS, LJUBLJANA',
-        '-7,40 EUR'
-      ],
-      [
-        'Bank inc.',
-        '21.09.2019',
-        'VI-INTERSPAR CITYPARK LJUBLJ, LJUBLJANA',
-        '-13,40 EUR'
-      ],
-      ['Bank inc.', '19.09.2019', 'VI-Joe Penas, Ljubljana', '-18,10 EUR'],
-      ['Bank inc.', '18.09.2019', 'KREDIT-903000823517523', '-218,79 EUR'],
-      ['SPL D.D.', '18.09.2019', 'DB SEP 2019', '-103,01 EUR'],
-      [
-        'Bank inc.',
-        '18.09.2019',
-        'Plačilna transakcija s SEPA direktno obremenitvijo',
-        '-0,33 EUR'
-      ],
-      ['TELEMACH D.O.O.', '18.09.2019', 'TELEMACH 9014444848085', '-43,00 EUR']
+      ['Bank inc.', current_date, 'some text', '-75,41 EUR'],
+      ['Bank inc.', current_date - 1.day, 'some text', '-13,04 EUR'],
+      ['Bank inc.', current_date - 2.days, 'some text', '-22,77 EUR'],
+      ['Bank inc.', current_date - 3.days, 'some text', '-5,74 EUR'],
+      ['Bank inc.', current_date - 4.days, 'some text', '-50,98 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-41,59 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-84,00 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-42,60 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-62,59 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '0,01 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-0,10 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-1,70 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-1,07 EUR'],
+      ['VZAJEMNA D.V.Z.', current_date - 5.days, 'some text', '-34,60 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-0,33 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-0,33 EUR'],
+      ['VRTEC VISKI GAJ', current_date - 5.days, 'OSKRBNINE', '-169,63 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-31,50 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-4,52 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-11,40 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-2,01 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-20,92 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-1,70 EUR'],
+      ['MARY SMITH', current_date - 5.days, 'Drugo', '450,78 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-3,40 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-68,00 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-7,40 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-13,40 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-18,10 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-218,79 EUR'],
+      ['SPL D.D.', current_date - 5.days, 'DB SEP 2019', '-103,01 EUR'],
+      ['Bank inc.', current_date - 5.days, 'some text', '-0,33 EUR'],
+      ['TELEMACH D.O.O.', current_date - 5.days, 'some text', '-43,00 EUR']
     ]
   end
 end
