39d95c2353c503f7210d9145fa0249cd15d4aeb5
Toni Dezman <toni.dezman@gmail.com>
2019-10-13 06:32:20 +0200

    annotates models and factories

--- spec/factories/expenses.rb
+++ spec/factories/expenses.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  month      :string
+#  year       :string
+#  expense_at :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 # typed: false
 FactoryBot.define do
   factory :expense do
--- spec/models/tenant_cost_spec.rb
+++ spec/models/tenant_cost_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: tenant_costs
+#
+#  month        :string
+#  year         :string
+#  expenses_sum :integer
+#  paid         :integer
+#  paid_at      :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 # typed: false
 require 'rails_helper'
 
