77ddf2f29c663de2d1733c63272cb955ca13d77c
Toni Dezman <toni.dezman@gmail.com>
2019-10-13 04:40:51 +0200

    adds indexes

--- db/migrate/20191013023349_create_expenses.rb
+++ db/migrate/20191013023349_create_expenses.rb
@@ -1,12 +1,13 @@
 class CreateExpenses < ActiveRecord::Migration[6.0]
   def change
     create_table :expenses do |t|
-      t.string :name
+      t.string   :name
+      t.string   :month
+      t.string   :year
       t.datetime :expense_at
-      t.string :month
-      t.string :year
-
       t.timestamps
     end
+
+    add_index(:expenses, [:month, :year])
   end
 end
--- db/migrate/20191013023507_create_tenant_costs.rb
+++ db/migrate/20191013023507_create_tenant_costs.rb
@@ -1,13 +1,14 @@
 class CreateTenantCosts < ActiveRecord::Migration[6.0]
   def change
-    create_table :tenant_costs do |t|
-      t.string :month
-      t.string :year
-      t.integer :expenses_sum
-      t.integer :paid
+    create_table :tenant_costs, id: false do |t|
+      t.string   :month
+      t.string   :year
+      t.integer  :expenses_sum
+      t.integer  :paid
       t.datetime :paid_at
-
       t.timestamps
     end
+
+    add_index :tenant_costs, [:month, :year], unique: true
   end
 end
