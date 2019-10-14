a59863481d8a811eacbcbc89d0025b4e8f12be77
Toni Dezman <toni.dezman@gmail.com>
2019-10-14 07:55:28 +0200

    working on Expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -18,4 +18,38 @@
 
 # typed: strong
 
-class Expense < ApplicationRecord; end
+class Expense < ApplicationRecord
+  validate :must_be_valid_expense
+  VALID_EXPENSES = %w[SPL RTV GEN-I TELEMACH]
+
+  def self.parse_expense_row(raw_expenses)
+    # ['GEN-I, D.O.O.', '03.10.2019', 'some text', '-52,00 EUR'],
+    result = []
+    raw_expenses.each do |raw_expense|
+      name, expense_at, _, raw_value = raw_expense
+      expense_at = expense_at.to_date
+      value = MoneyParser.parse(raw_value)
+      result <<
+        Expense.new(
+          name: name,
+          expense_at: expense_at,
+          value: value,
+          month: expense_at.month,
+          year: expense_at.year
+        )
+    end
+    result
+  end
+
+  def must_be_valid_expense
+    is_valid_expense = false
+
+    VALID_EXPENSES.each do |expense|
+      is_valid_expense = true if name.upcase.include? expense
+    end
+
+    errors.add(:name, 'this is not valid expense') unless is_valid_expense
+  end
+end
+
+class InvalidExpense < StandardError; end
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -22,11 +22,41 @@ require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
   describe '.parse_expense_row' do
-    it 'correctly parses raw record data from the scraper'
-    it 'raises error if too little elements'
-    it 'raises error if to much elements'
-    it 'raises error if tenant expense value is positive integer'
-    it 'raises error if tenant expense value is zero'
+    it 'correctly parses raw record data from the scraper' do
+      expect { Expense.parse_expense_row(raw_expenses) }.not_to raise_error(
+                      InvalidExpense
+                    )
+    end
+
+    it 'converts raw expense data to Expense objects' do
+      expect(
+        Expense.parse_expense_row(raw_expenses).all? { |expense|
+          expense.is_a? Expense
+        }
+      ).to be true
+    end
+
+    it 'saves expense only once (idempotent) on multiple runs' do
+    end
+
+    it 'gets back only valid tenant expenses' do
+      expenses = Expense.parse_expense_row(raw_expenses)
+      counter = 0
+      expenses.each { |expense| counter += 1 if expense.valid? }
+      expect(counter).to eq(4)
+    end
+
+    it 'raises error if too little elements' do
+    end
+
+    it 'raises error if to much elements' do
+    end
+
+    it 'raises error if tenant expense value is positive integer' do
+    end
+
+    it 'raises error if tenant expense value is zero' do
+    end
   end
 
   describe 'expenses are idempotent' do
@@ -34,43 +64,50 @@ RSpec.describe Expense, type: :model do
     it 'correctly saves expenses only once on multiple runs'
   end
 
-  def expenses(current_date: DateTime.now.beginning_of_month + 20.days)
+  def raw_date(days_ago, current_date: nil)
+    if current_date.nil?
+      current_date = DateTime.now.beginning_of_month + 20.days
+    end
+    (current_date - days_ago).strftime('%d.%m.%Y')
+  end
+
+  def raw_expenses
     [
-      ['Bank inc.', current_date, 'some text', '-75,41 EUR'],
-      ['Bank inc.', current_date - 1.day, 'some text', '-13,04 EUR'],
-      ['Bank inc.', current_date - 2.days, 'some text', '-22,77 EUR'],
-      ['Bank inc.', current_date - 3.days, 'some text', '-5,74 EUR'],
-      ['Bank inc.', current_date - 4.days, 'some text', '-50,98 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-41,59 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-84,00 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-42,60 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-62,59 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '0,01 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-0,10 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-1,70 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-1,07 EUR'],
-      ['VZAJEMNA', current_date - 5.days, 'some text', '-34,60 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-0,33 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-0,33 EUR'],
-      ['VRTEC', current_date - 5.days, 'OSKRBNINE', '-169,63 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-31,50 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-4,52 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-11,40 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-2,01 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-20,92 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-1,70 EUR'],
-      ['MARY SMITH', current_date - 5.days, 'Drugo', '450,78 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-3,40 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-68,00 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-7,40 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-13,40 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-18,10 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-218,79 EUR'],
-      ['SPL D.D.', current_date - 5.days, 'DB SEP 2019', '-103,01 EUR'],
-      ['Bank inc.', current_date - 5.days, 'some text', '-0,33 EUR'],
-      ['TELEMACH D.O.O.', current_date - 2.days, 'some text', '-43,00 EUR']
-      ['GEN-I, D.O.O.', current_date - 2.days, 'some text', '-52,00 EUR']
-      ['RTV SLOVENIJA', current_date - 3.days, 'some text', '-12,75 EUR']
+      ['Bank inc.', raw_date(0.days), 'some text', '-75,41 EUR'],
+      ['Bank inc.', raw_date(1.days), 'some text', '-13,04 EUR'],
+      ['Bank inc.', raw_date(2.days), 'some text', '-22,77 EUR'],
+      ['Bank inc.', raw_date(3.days), 'some text', '-5,74 EUR'],
+      ['Bank inc.', raw_date(4.days), 'some text', '-50,98 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-41,59 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-84,00 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-42,60 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-62,59 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '0,01 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-0,10 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-1,70 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-1,07 EUR'],
+      ['VZAJEMNA', raw_date(5.days), 'some text', '-34,60 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-0,33 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-0,33 EUR'],
+      ['VRTEC', raw_date(5.days), 'OSKRBNINE', '-169,63 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-31,50 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-4,52 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-11,40 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-2,01 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-20,92 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-1,70 EUR'],
+      ['MARY SMITH', raw_date(5.days), 'Drugo', '450,78 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-3,40 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-68,00 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-7,40 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-13,40 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-18,10 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-218,79 EUR'],
+      ['SPL D.D.', raw_date(5.days), 'DB SEP 2019', '-103,01 EUR'],
+      ['Bank inc.', raw_date(5.days), 'some text', '-0,33 EUR'],
+      ['TELEMACH D.O.O.', raw_date(2.days), 'some text', '-43,00 EUR'],
+      ['GEN-I, D.O.O.', raw_date(2.days), 'some text', '-52,00 EUR'],
+      ['RTV SLOVENIJA', raw_date(3.days), 'some text', '-12,75 EUR']
     ]
   end
 end
