e0ec23a07a3499129dc27d7edd3eaeb93d646e3a
Toni Dezman <toni.dezman@gmail.com>
2019-11-20 17:21:47 +0100

    small changes

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,7 @@
 class ExpensesController < ApplicationController
   def index
+    time = Time.now
+    @year_month = "#{time.year}-#{time.month}"
     @current_month_expenses = Expense.print_current_month_expenses
   end
 end
